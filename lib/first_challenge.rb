421c7b9ff51ab6d1ce996e96dd60f2f273573bcc
Moses Olawoyin <m.bastford@gmail.com>
2019-02-07 23:23:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def first_challenge
   contacts = {
     "Jon Snow" => {
@@ -13,10 +14,16 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+  contacts.each do |attribute, value|
+    pry
+    if attribute == favorite_icecream_flavors
+      value.each do |flavor|
+        if flavor == "strawberry"
+          flavor.delete("strawberry")
+        end
+      end
+    end
+  end
+    contacts
 end
 
